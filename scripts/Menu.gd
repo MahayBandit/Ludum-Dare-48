95746b43206536ba9b58c21267c0b49cddbb6827
MatiF100 <mateuszfesz@wp.pl>
2021-04-25 15:58:06 +0200

    Added main menu

---
+++ scripts/Menu.gd
@@ -0,0 +1,31 @@
+extends TextureRect
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Exit_pressed():
+	get_tree().quit()
+	pass 
+
+
+func _on_Start_pressed():
+	get_tree().change_scene("res://scenes/Main.tscn")
+	pass 
+
+
+func _on_Settings_B_pressed():
+	print("Not yet implemented")
+	pass # Replace with function body.
