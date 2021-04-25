299855c913eb348ebb1c80d48891b516f3b6893b
Luna <73404617+Stefan-Luna-Domaradzki@users.noreply.github.com>
2021-04-25 11:26:14 +0200

    Kolizja z przeciwnikiem
    działa ( OwO )
    wrzucam ten prosty skrypt żeby go nie stracić

--- scripts/Char_Player.gd
+++ scripts/Char_Player.gd
@@ -3,6 +3,11 @@ extends KinematicBody2D
 export var speed:= 0
 export var falling_modifier = 1.0
 
+export var health = 0
+
+func _ready():
+	print("fuck this shit")
+
 func _physics_process(delta: float) -> void:
 	
 	var movement: = Vector2(1,1)
@@ -28,3 +33,11 @@ func _physics_process(delta: float) -> void:
 	move_and_slide(movement)
 
 
+func take_dmg():
+	print("fuck this shit")
+	
+	health = health - 1
+	if health <= 0:
+		visible = not visible
+	
+	
